a8006eda083555482dfb271f812dcc121899f026
Abdul Vahid K P <abdulvahid1117@gmail.com>
2023-05-26 11:44:29 +0530

    name changed to weare

--- frontend/src/components/AdminNav.jsx
+++ frontend/src/components/AdminNav.jsx
@@ -15,7 +15,7 @@ function UserNavbar({data}) {
       <nav className="px-2 py-2.5 shadow-md ">
         <div className="w-screen sm:mt-0 sm:container flex flex-wrap justify-between items-center mx-auto">
           <div className="">
-            <span className="text-bold text-xl sm:text-3xl italic font-semibold self-center cursor-pointer select-none">S R V - admin</span>
+            <span className="text-bold text-xl sm:text-3xl italic font-semibold self-center cursor-pointer select-none">WEARE Admin</span>
           </div>
           <div className="items-center">
             <CSVLink className="bg-blue-700 text-white font-[Poppins] duration-500 p-1 sm:px-4 text-xs sm:text-lg sm:py-2 mx-1 sm:mx-4 hover:bg-blue-800 rounded" data={data} onClick={()=> {}} >EXPORT USERLIST</CSVLink>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function UserNavbar() {
       <nav className="px-2 py-2.5 shadow-md ">
         <div className="w-screen sm:mt-0 sm:container flex flex-wrap justify-between items-center mx-auto">
           <div className="">
-            <span className="text-bold text-xl sm:text-3xl italic font-semibold self-center cursor-pointer select-none">S R V</span>
+            <span className="text-bold text-xl sm:text-3xl italic font-semibold self-center cursor-pointer select-none">WEARE!</span>
           </div>
           <div className="items-center">
             <button
